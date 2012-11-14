0ef127500ede0bb392f0986f321688d2318494e8
cospih <cioan@pih.org>
2012-11-14 10:29:51 -0500

    Retrieve the location from the session.

--- omod/src/main/java/org/openmrs/module/emr/page/controller/ActiveVisitsPageController.java
+++ omod/src/main/java/org/openmrs/module/emr/page/controller/ActiveVisitsPageController.java
@@ -2,23 +2,20 @@ package org.openmrs.module.emr.page.controller;
 
 import org.openmrs.Location;
 import org.openmrs.api.LocationService;
+import org.openmrs.module.emr.EmrContext;
 import org.openmrs.module.emr.adt.AdtService;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.page.PageModel;
 
 public class ActiveVisitsPageController {
 	
-	public void get(PageModel model, @SpringBean AdtService service,
+	public void get(EmrContext emrContext, PageModel model, @SpringBean AdtService service,
 	                @SpringBean("locationService") LocationService locationService) {
 		
-		// TEMPORARY HACK UNTIL WE HAVE A SESSION LOCATION TO START FROM=
-		Location visitLocation = null;
-		for (Location candidate : locationService.getAllLocations(false)) {
-			Location test = service.getLocationThatSupportsVisits(candidate);
-			if (test != null) {
-				visitLocation = test;
-				break;
-			}
+		Location sessionLocation = emrContext.getSessionLocation();
+		Location visitLocation=null;
+		if(sessionLocation!=null){
+			visitLocation= service.getLocationThatSupportsVisits(sessionLocation);
 		}
 		if (visitLocation == null) {
 			throw new IllegalStateException("Configuration required");
