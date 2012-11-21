7986e747e55cd12eb45ee7a2ad0cce0c9cb31bc1
Mário Areias <mario.s.s.areias@gmail.com>
2012-11-21 14:02:56 -0200

    still trying to create a integration test to generate dossier numbers

--- api/src/test/java/org/openmrs/module/emr/integration/PaperRecordServiceIntegrationTest.java
+++ api/src/test/java/org/openmrs/module/emr/integration/PaperRecordServiceIntegrationTest.java
@@ -1,10 +1,10 @@
 package org.openmrs.module.emr.integration;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openmrs.Location;
 import org.openmrs.Patient;
+import org.openmrs.PatientIdentifier;
 import org.openmrs.api.AdministrationService;
 import org.openmrs.api.PatientService;
 import org.openmrs.module.emr.EmrConstants;
@@ -14,34 +14,42 @@ import org.openmrs.module.idgen.service.IdentifierSourceService;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.openmrs.test.SkipBaseSetup;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.NotTransactional;
 
+import javax.annotation.Resource;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.notNull;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
-@Ignore
 @SkipBaseSetup
 public class PaperRecordServiceIntegrationTest extends BaseModuleContextSensitiveTest {
-
-    private AdministrationService administrationService;
 
     @Autowired
     private IdentifierSourceService identifierSourceService;
 
     @Autowired
-    private PatientService patientService;
+    @Qualifier("patientServiceTest")
+    private PatientService patientServiceStub;
+
+    private AdministrationService administrationService;
 
     private PaperRecordService paperRecordService;
 
 
     @Before
     public void setUp(){
+        patientServiceStub = spy(patientServiceStub);
         paperRecordService = new PaperRecordServiceImpl();
         administrationService = mock(AdministrationService.class);
         ((PaperRecordServiceImpl) paperRecordService).setAdministrationService(administrationService);
         ((PaperRecordServiceImpl) paperRecordService).setIdentifierSourceService(identifierSourceService);
-        ((PaperRecordServiceImpl) paperRecordService).setPatientService(patientService);
+        ((PaperRecordServiceImpl) paperRecordService).setPatientService(patientServiceStub);
     }
 
     @Override
@@ -62,7 +70,9 @@ public class PaperRecordServiceIntegrationTest extends BaseModuleContextSensitiv
         authenticate();
         when(administrationService.getGlobalProperty(EmrConstants.GP_PAPER_RECORD_IDENTIFIER_TYPE)).thenReturn("e66645eb-03a8-4991-b4ce-e87318e37566");
 
-        String paperMedicalRecordNumberFor = paperRecordService.createPaperMedicalRecordNumberFor(mock(Patient.class), new Location(15));
+        doNothing().when(patientServiceStub).savePatientIdentifier(any(PatientIdentifier.class));
+
+        String paperMedicalRecordNumberFor = paperRecordService.createPaperMedicalRecordNumberFor(new Patient(), new Location(15));
      }
 
 }
