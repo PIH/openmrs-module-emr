5d87d1b9c97ff7c3f158c74c237e328efd8710b3
Alexandre Barbosa <abarbos@thoughtworks.com>
2013-01-03 18:14:41 -0200

    #143: Spliting navigator.js in multiple files

---
+++ omod/src/main/webapp/resources/scripts/navigator/navigator.js
@@ -0,0 +1,57 @@
+function initFormModels() {
+    var sections = _.map($('section'), function(s) {
+        return SectionModel(s);
+    });
+
+    sections[0].toggleSelection();
+    return sections;
+}
+
+function initKeyboardHandlersChain() {
+    var sections = initFormModels();
+    var sectionsHandler = SectionsHandler();
+    _.each(sections, function(s) { sectionsHandler.addSection(s); });
+
+    var questions = _.flatten( _.map(sections, function(s) { return s.questions; }), true);
+    var questionsHandler = QuestionsHandler(sectionsHandler);
+    _.each(questions, function(q) { questionsHandler.addQuestion(q); });
+
+    var fields = _.flatten(_.map(questions, function(q) { return q.fields; }), true);
+    var fieldsHandler = FieldsHandler(questionsHandler);
+    _.each(fields, function(f) { fieldsHandler.addField(f); });
+
+    return fieldsHandler;
+}
+
+function KeyboardController() {
+    var handlerChainRoot = initKeyboardHandlersChain();
+
+    $('body').keydown(function(key) {
+        switch(key.which) {
+            case 39:
+                handlerChainRoot.handleRightKey() && key.preventDefault();
+                break;
+            case 37:
+                handlerChainRoot.handleLeftKey() && key.preventDefault();
+                break;
+            case 38:
+                handlerChainRoot.handleUpKey() && key.preventDefault();
+                break;
+            case 40:
+                handlerChainRoot.handleDownKey() && key.preventDefault();
+                break;
+            case 27:
+                handlerChainRoot.handleEscKey() && key.preventDefault();
+                break;
+            case 9:
+                if(event.shiftKey) {
+                    handlerChainRoot.handleShiftTabKey() && key.preventDefault();
+                } else {
+                    handlerChainRoot.handleTabKey() && key.preventDefault();
+                }
+                break;
+            default:
+                break;
+        }
+    });
+}
