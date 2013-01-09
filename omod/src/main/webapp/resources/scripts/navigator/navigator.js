e8fd0e17573cc5aa7fa67e28ee3ab3c982a0ec19
Alexandre Barbosa <abarbos@thoughtworks.com>
2013-01-08 22:12:21 -0200

    #143: Draft on mechanism to keep focused question on same position

--- omod/src/main/webapp/resources/scripts/navigator/navigator.js
+++ omod/src/main/webapp/resources/scripts/navigator/navigator.js
@@ -1,6 +1,8 @@
 function initFormModels() {
     var formElement = $('div#content > form').first();
+    formElement.prepend('<div id="spacer"></div>');
     formElement.prepend('<div class="formBreadcrumb"></div>');
+    var spacer = formElement.find('div#spacer').first();
     var breadcrumb = formElement.find('div.formBreadcrumb').first();
 
     var sections = _.map(formElement.find('section'), function(s) {
@@ -17,7 +19,7 @@ function initFormModels() {
 
     var questions = _.flatten( _.map(sections, function(s) { return s.questions; }), true);
     var fields = _.flatten(_.map(questions, function(q) { return q.fields; }), true);
-    return [sections, questions, fields];
+    return [sections, questions, fields, spacer];
 }
 
 function initKeyboardHandlersChain(sections, questions, fields) {
@@ -41,10 +43,13 @@ function initMouseHandlers(sections, questions, fields) {
 
 function KeyboardController() {
     var modelsList = initFormModels();
-    var sections=modelsList[0], questions=modelsList[1], fields=modelsList[2];
+    var sections=modelsList[0], questions=modelsList[1], fields=modelsList[2], spacer=modelsList[3];
     initMouseHandlers(sections, questions, fields);
     var handlerChainRoot = initKeyboardHandlersChain(sections, questions, fields);
+    var spacerUpdater = SpacerUpdater(spacer, sections);
+
     handlerChainRoot.handleTabKey();
+    spacerUpdater.update();
 
     $('body').keydown(function(key) {
         switch(key.which) {
@@ -69,6 +74,7 @@ function KeyboardController() {
                 } else {
                     handlerChainRoot.handleTabKey();
                 }
+                spacerUpdater.update();
                 key.preventDefault()
                 break;
             case 13:
@@ -77,4 +83,32 @@ function KeyboardController() {
                 break;
         }
     });
+}
+
+function SpacerUpdater(spacer, sectionModels) {
+    var spacerEl = spacer;
+    var sections = sectionModels;
+    var questionsBefore = 2;
+    var questionHeight = 40;
+
+    var api = {};
+    api.update = function() {
+        var currentSection = _.find(sections, function(s) { return s.isSelected; });
+        var currentFieldIndex = 0;
+        var currentField = _.find(currentSection.questions, function(q, i) { currentFieldIndex = i; return q.isSelected; });
+
+
+        _.each(currentSection.questions, function(q,i) {
+            if(i < (currentFieldIndex-questionsBefore) ) {
+                q.hide();
+            } else {
+                q.show();
+            }
+        });
+
+        spacerEl.css('height', questionHeight*(questionsBefore-currentFieldIndex));
+        console.log(currentField.questionLegend().css('height'));
+    }
+
+    return api;
 }
