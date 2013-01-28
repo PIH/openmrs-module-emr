573828e52fdb84fabd73877f2d87c5e4f84be83e
Glauber Ramos <glauberamos@gmail.com>
2013-01-28 11:02:44 -0500

    #281 fixing tests

--- omod/src/test/webapp/resources/scripts/emrTests.js
+++ omod/src/test/webapp/resources/scripts/emrTests.js
@@ -35,7 +35,7 @@ describe("Tests of emr functions", function() {
 
         emr.successAlert("some success message");
 
-        expect(jqueryWithSpy.toastmessage).toHaveBeenCalledWith('showToast', { type : 'success', position : 'top-right', text : 'some success message', close: null });
+        expect(jqueryWithSpy.toastmessage).toHaveBeenCalledWith('showToast', { type : 'success', position : 'top-right', text : 'some success message', stayTime: 8000, close: null });
 
     });
 
@@ -48,7 +48,7 @@ describe("Tests of emr functions", function() {
 
         emr.errorAlert("some error message");
 
-        expect(jqueryWithSpy.toastmessage).toHaveBeenCalledWith('showToast', { type : 'error', position : 'top-right', text : 'some error message', close: null });
+        expect(jqueryWithSpy.toastmessage).toHaveBeenCalledWith('showToast', { type : 'error', position : 'top-right', text : 'some error message', stayTime: 8000, close: null });
 
     });
 
