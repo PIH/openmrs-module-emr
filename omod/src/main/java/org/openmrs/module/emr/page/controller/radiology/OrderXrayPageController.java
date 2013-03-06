4c43d44223696f2b2b19e406c151b267b838f411
mogoodrich <mgoodrich@pih.org>
2013-03-06 17:54:58 -0500

    #411: Location names in order radiology dropdown are not formatted into French

--- omod/src/main/java/org/openmrs/module/emr/page/controller/radiology/OrderXrayPageController.java
+++ omod/src/main/java/org/openmrs/module/emr/page/controller/radiology/OrderXrayPageController.java
@@ -22,6 +22,7 @@ import org.openmrs.api.ConceptService;
 import org.openmrs.api.LocationService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.emr.EmrProperties;
+import org.openmrs.module.emr.api.EmrService;
 import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.annotation.SpringBean;
@@ -30,15 +31,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 
 public class OrderXrayPageController {
 
     public void controller(@RequestParam("patientId") Patient patient,
-                           @SpringBean("conceptService") ConceptService conceptService,
                            @SpringBean("emrProperties") EmrProperties emrProperties,
-                           @SpringBean("locationService") LocationService locationService,
+                           @SpringBean("emrService") EmrService emrService,
                            UiUtils ui,
                            PageModel model) {
 
@@ -46,17 +48,26 @@ public class OrderXrayPageController {
         model.addAttribute("currentProvider", providers.iterator().next());
 
         model.addAttribute("xrayOrderables", ui.toJson(getXrayOrderables(emrProperties, Context.getLocale())));
-        model.addAttribute("portableLocations", ui.toJson(getPortableLocations(locationService)));
+        model.addAttribute("portableLocations", ui.toJson(getPortableLocations(emrService, ui)));
         model.addAttribute("patient", patient);
     }
 
-    private List<SimpleObject> getPortableLocations(LocationService locationService) {
+    private List<SimpleObject> getPortableLocations(EmrService emrService, final UiUtils ui) {
         List<SimpleObject> items = new ArrayList<SimpleObject>();
-        List<Location> locations = locationService.getAllLocations(false);
+        List<Location> locations = emrService.getLoginLocations(); // TODO: is login locations really the right thing here?
+
+        // sort objects by localized name
+        Collections.sort(locations, new Comparator<Location>() {
+            @Override
+            public int compare(Location location1, Location location2) {
+                return ui.format(location1).compareTo(ui.format(location2));
+            }
+        });
+
         for (Location location: locations) {
             SimpleObject item = new SimpleObject();
             item.put("value", location.getLocationId());
-            item.put("label", location.getName());
+            item.put("label", ui.format(location));
             items.add(item);
         }
         return items;
