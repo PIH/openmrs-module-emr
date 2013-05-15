c7d502949f79c53270b41d73596b076b579da515
mogoodrich <mgoodrich@pih.org>
2013-05-15 14:31:43 -0400

    deleted mistakenly added DispositionActionExtender

--- api/src/main/java/org/openmrs/module/emr/htmlformentry/DispositionActionsExtender.java
+++
@@ -1,8 +0,0 @@
-package org.openmrs.module.emr.htmlformentry;
-
-/**
- * Extends HFE so that on form submittal the encounter is scanned for any Dispositions and then
- * triggers any disposition actions as need be
- */
-public class DispositionActionsExtender {
-}
